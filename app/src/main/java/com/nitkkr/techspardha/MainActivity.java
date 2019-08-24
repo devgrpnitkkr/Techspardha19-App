e069cd7eb784346e7cd1b1a31210aa24b5a69000
Shivam Dhammi <dhammishivam@gmail.com>
2019-08-24 16:41:24 +0530

    BottomNavigation bar in Main

--- app/src/main/java/com/nitkkr/techspardha/MainActivity.java
+++ app/src/main/java/com/nitkkr/techspardha/MainActivity.java
@@ -1,8 +1,13 @@
 package com.nitkkr.techspardha;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,39 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		BottomNavigationView bottomNavigationView = findViewById(R.id.main_bottom_navigation_view);
+		bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
+		getSupportFragmentManager().beginTransaction().replace(R.id.main_fragment_container,
+				new FragmentHome()).commit();
+
 	}
+
+	private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+			new BottomNavigationView.OnNavigationItemSelectedListener() {
+				@Override
+				public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+					Fragment selectedFragment = null;
+
+					switch (menuItem.getItemId()){
+						case R.id.nav_home:
+							selectedFragment = new FragmentHome();
+							break;
+						case R.id.nav_sponsi:
+							selectedFragment = new FragmentSponsership();
+							break;
+						case R.id.nav_food:
+							selectedFragment = new FragmentFood();
+							break;
+					}
+
+					getSupportFragmentManager().beginTransaction().replace(R.id.main_fragment_container,
+							selectedFragment).commit();
+
+					return true;
+
+//					return false;
+				}
+			};
 }
